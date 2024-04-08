296045185a0777dc40f86794ff8c409eeb315fc3
joaolfp <joaolucasfp2001@gmail.com>
2024-04-08 00:09:50 -0300

    Separate code in another methods

--- src/encode_base64.rs
+++ src/encode_base64.rs
@@ -10,28 +10,40 @@ pub fn encode_base64(data: &str) -> String {
         let mut base64_indexes = Vec::new();
         let mut padding_count = 0;
 
-        for i in 0..3 {
-            if index < data_bytes.len() {
-                bytes[i] = data_bytes[index];
-                index += 1;
-            } else {
-                padding_count += 1;
-            }
-        }
-
-        base64_indexes.push((bytes[0] >> 2) as u8);
-        base64_indexes.push((((bytes[0] & 0x03) << 4) | ((bytes[1] & 0xF0) >> 4)) as u8);
-        base64_indexes.push((((bytes[1] & 0x0F) << 2) | ((bytes[2] & 0xC0) >> 6)) as u8);
-        base64_indexes.push((bytes[2] & 0x3F) as u8);
-
-        for i in 0..(4 - padding_count) {
-            result.push(base64_chars[base64_indexes[i] as usize] as char);
-        }
-
-        for _ in 0..padding_count {
-            result.push('=');
-        }
+        index = count_number_bytes(index, data_bytes, &mut bytes, &mut padding_count);
+        calculate_base64_indexes(&bytes, &mut base64_indexes);
+        append_base64_chars(&mut result, &base64_chars, &base64_indexes, padding_count);
     }
 
     result
+}
+
+fn count_number_bytes(mut index: usize, data_bytes: &[u8], bytes: &mut [u8; 3], padding_count: &mut usize) -> usize {
+    for i in 0..3 {
+        if index < data_bytes.len() {
+            bytes[i] = data_bytes[index];
+            index += 1;
+        } else {
+            *padding_count += 1;
+        }
+    }
+    
+    index
+}
+
+fn calculate_base64_indexes(bytes: &[u8; 3], base64_indexes: &mut Vec<u8>) {
+    base64_indexes.push((bytes[0] >> 2) as u8);
+    base64_indexes.push((((bytes[0] & 0x03) << 4) | ((bytes[1] & 0xF0) >> 4)) as u8);
+    base64_indexes.push((((bytes[1] & 0x0F) << 2) | ((bytes[2] & 0xC0) >> 6)) as u8);
+    base64_indexes.push((bytes[2] & 0x3F) as u8);
+}
+
+fn append_base64_chars(result: &mut String, base64_chars: &[u8], base64_indexes: &[u8], padding_count: usize) {
+    for i in 0..(4 - padding_count) {
+        result.push(base64_chars[base64_indexes[i] as usize] as char);
+    }
+
+    for _ in 0..padding_count {
+        result.push('=');
+    }
 }
